55851abed892b8fc9504071c81d818bc6bf27e79
Piotr Jankiewicz <01128355@pw.edu.pl>
2024-01-02 17:36:44 +0100

    dzis deadline

--- proj2/test0.m
+++ proj2/test0.m
@@ -9,6 +9,9 @@ function [] = test0()
 % WYJŚCIE:
 %   -
 
+tol = 10^-8;
+LIMIT = 10^3;
+
 fprintf("Testy działania poprawności dla macierzy 10 na 10. \nWartości własne macierzy to 1,2,3,4,5,6,7,10.\n")
 A = [
     10 1 0 0 0 0 0 0 0 0; 
@@ -26,32 +29,8 @@ upper_diag = diag(A,1); % upper diagonal elements
 lower_diag = diag(A,-1); % lower diagonal elements
 
 tic
-fprintf("Test 1 - poprwanosc wyniku dla miu=6.4 (oczekiwana nabliższa wartosc wlasna to 6)\n");
-[l,x, i] = P2Z46_InversePowerMethod(10, main_diag,upper_diag, lower_diag, 6.4);
-fprintf("   - wynik lambda=%d, l.iteracji=%d\n", [l,i]);
-toc
-
-
-fprintf("\n");
-tic
-fprintf("Test 2 - poprwanosc wyniku dla miu=6.6 (oczekiwana nabliższa wartosc wlasna to 7)\n");
-[l,x, i] = P2Z46_InversePowerMethod(10, main_diag,upper_diag, lower_diag, 6.6);
-fprintf("   - wynik lambda=%d, l.iteracji=%d\n", [l,i]);
-toc
-
-
-fprintf("\n");
-tic
-fprintf("Test 3 - poprwanosc wyniku dla miu=1 (oczekiwana nabliższa wartosc wlasna to 1)\n");
-[l,x, i] = P2Z46_InversePowerMethod(10, main_diag,upper_diag, lower_diag, 1);
-fprintf("   - wynik lambda=%d, l.iteracji=%d\n", [l,i]);
-toc
-
-
-fprintf("\n");
-toc
-fprintf("Test 4 - poprwanosc wyniku dla miu=3.1 (oczekiwana nabliższa wartosc wlasna to 3)\n");
-[l,x, i] = P2Z46_InversePowerMethod(10, main_diag,upper_diag, lower_diag, 3.1);
+fprintf("Test 1 - poprwanosc wyniku - oczekiwana najmniejsza wartosc wlasna to 1\n");
+[l,x, i] = P2Z14_InversePowerMethod(10, main_diag,upper_diag, lower_diag,tol, LIMIT);
 fprintf("   - wynik lambda=%d, l.iteracji=%d\n", [l,i]);
 toc
 
--- proj2/test5.m
+++ proj2/test5.m
@@ -9,6 +9,10 @@ function [] = test5()
 % WYJŚCIE:
 %   -
 
+
+tol = 10^-8;
+LIMIT = 10^3;
+
 fprintf("Testy działania poprawności dla macierzy 2000 na 2000.\n")
 n=2000;
 low = -99;
@@ -16,11 +20,10 @@ high = +99;
 diagonal = low + (high-low) .* rand(1, n);
 diagLower = low + (high-low) .* rand(1, n-1);
 diagUpper = low + (high-low) .* rand(1, n-1);
-miu  = low + (high-low) .* rand(1, 1);
 
 tic
-fprintf("Test 1 - poprwanosc wyniku dla miu=%d\n", [miu]);
-[l,x, i] = P2Z46_InversePowerMethod(n, diagonal,diagLower, diagUpper, miu);
+fprintf("Test 1 - poprwanosc wyniku dla\n");
+[l,x, i] = P2Z14_InversePowerMethod(n, diagonal,diagLower, diagUpper, tol, LIMIT);
 fprintf("   - wynik lambda=%d, l.iteracji=%d\n", [l,i]);
 toc
 
