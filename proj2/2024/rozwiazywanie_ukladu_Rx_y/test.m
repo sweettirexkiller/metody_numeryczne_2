76b0aef93f3f44efd4769c9def1b60475abe394d
Piotr Tadeusz Józef Jankiewicz <piotrjankiewicz02@gmail.com>
2025-01-19 16:29:36 +0100

    testy dla rozwiazywania gaussem

--- proj2/2024/rozwiazywanie_ukladu_Rx_y/test.m
+++ proj2/2024/rozwiazywanie_ukladu_Rx_y/test.m
@@ -2,49 +2,57 @@ function test()
 % Projekt 2, zadanie 14
 % Piotr Jankiewicz, 288767
 %
-% Test dla funkcji eliminacji Gaussa dla macierzy trójdiagonalnej
+% Test sprawdza poprawność działania funkcji trojdiagonalny_gauss
+% rozwiązującej układ równań R * x = y z macierzą trójdiagonalną R
+% oraz funkcji mnozenie_trojdiagonal_wektor dokonującej mnożenia
+% macierzy trójdiagonalnej przez wektor.
 %
-% Testuje funkcje:
-% 1. trojdiagonalny_gauss - rozwiązanie układu R * x = y, gdzie R jest macierzą trójdiagonalną.
-% 2. mnozenie_trojdiagonal_wektor - weryfikacja wyników przez ponowne mnożenie macierzy R przez x.
+% Weryfikacja polega na rozwiązaniu układu R * x = y dla zadanej
+% macierzy R i wektora y, a następnie pomnożeniu otrzymanego x
+% przez macierz R. Jeśli funkcje działają poprawnie, uzyskany wektor
+% y_test powinien być taki sam jak wejściowy y.
 %
-% Testowane właściwości:
-% - Poprawność rozwiązania układu R * x = y.
-% - Norma różnicy norm(y - y_test) powinna być bliska zeru.
+% WEJŚCIE:
+% - Macierz R w postaci wektorów p, q, s zawierających odpowiednio
+%   elementy głównej przekątnej, pierwszej i drugiej przekątnej nad główną
+% - Wektor prawych stron równania y
+%
+% WYJŚCIE:
+% - Komunikat o wyniku testu
 
-    % Dane wejściowe
-    fprintf("Testy poprawności funkcji eliminacji Gaussa dla macierzy R (5x5):\n");
-    p = [4, 2, 1, 2, 3]; % Główna przekątna
-    q = [1, 2, 3, 4];    % Pierwsza przekątna nad główną
-    s = [2, 3, 4];       % Druga przekątna nad główną
+    % Definicja danych wejściowych
+    p = [4, 2, 1, 2, 3];  % Główna przekątna
+    q = [1, 2, 3, 4];     % Pierwsza przekątna nad główną
+    s = [2, 3, 4];        % Druga przekątna nad główną
+    y = [1, 1, 1, 1, 1]; % Wektor prawych stron
 
-    % Konstrukcja macierzy trójdiagonalnej R (dla weryfikacji)
+    disp('Test funkcji trojdiagonalny_gauss i mnozenie_trojdiagonal_wektor');
+    disp('dla układu równań R * x = y z macierzą trójdiagonalną R (5x5).');
+    disp(' ');
+
+    % Konstrukcja macierzy R z zadanych wektorów
     R = diag(p) + diag(q, 1) + diag(s, 2);
-    disp("Macierz trójdiagonalna R:");
+    disp('Macierz trójdiagonalna R:');
     disp(R);
+    disp('Wektor prawych stron y:');
+    disp(y);
+    disp('Naciśnij dowolny klawisz, aby kontynuować...');
+    pause;
 
-    % Wektor prawej strony równania
-    y = [1, 1, 1, 1, 1];
-
-    % Rozwiązanie układu R * x = y za pomocą własnej funkcji
+    % Rozwiązanie układu R * x = y
+    disp('Rozwiązywanie układu R * x = y za pomocą trojdiagonalny_gauss...');
     x = trojdiagonalny_gauss(p, q, s, y);
-    fprintf("Wynik własnej funkcji dla R * x = y:\n");
-    disp(x);
+    disp('Otrzymany wektor x:');
+    disp(x');
 
     % Weryfikacja przez mnożenie R * x
+    disp('Weryfikacja wyniku przez mnożenie R * x za pomocą mnozenie_trojdiagonal_wektor...');
     y_test = mnozenie_trojdiagonal_wektor(p, q, s, x);
 
-    % Obliczenie normy różnicy
-    norma_difference = norm(y - y_test);
+    % Obliczenie normy różnicy między y a y_test
+    norma_diff = norm(y - y_test);
+    disp('|| y - y_test || = ');
+    disp(norma_diff);
+    disp(' ');
 
-    % Wyniki testu
-    fprintf("Norma różnicy między y a y_test: %.15f\n", norma_difference);
-
-    % Podsumowanie testu
-    fprintf("\nPodsumowanie testu:\n");
-    if norma_difference < 1e-10
-        fprintf("Test zakończony sukcesem. Wyniki są zgodne z oczekiwaniami.\n");
-    else
-        fprintf("Test nieudany. Wyniki różnią się od oczekiwanych.\n");
-    end
-end
+end  % function
